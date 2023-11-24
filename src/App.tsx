358d6d7aa463e2a77ceeec8d40f66daacf747564
imparassharma <parasangiras121@gmail.com>
2023-11-24 14:00:03 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import "./Style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+
+    const [todo,setTodo] = useState<string>("");
+    const [task,setTask] = useState<string[]>([]);
+    interface props{
+        todo:string;
+        setTodo:React.Dispatch<React.SetStateAction<string>>;
+    }
+
+    const handleChange= (e:React.SyntheticEvent)=> {
+        const inputVal = (e.target as HTMLInputElement).value;
+        if(inputVal!=""){
+            setTodo(inputVal);
+        }
+    }
+
+    const handleSubmit= (event: React.FormEvent)=> {
+        event.preventDefault();
+        if(todo!=""){
+            setTask(prevTasks => [...prevTasks, todo]);
+            console.log(task);
+            setTodo("");
+        }
+        (event.target as HTMLInputElement).value = "";
+    }
+
+    
+    const handleValue= (event:React.SyntheticEvent)=> {
+        (event.target as HTMLInputElement).value = "";
+    }
+
+
+    return(
+        <div className='mainContainer'>
+            <form className='formContainer' onSubmit={handleSubmit}>
+                <label>Tasks</label>
+                <input className='inputTask' id='input' onChange={handleChange} value={todo}></input>
+                <button className='btn' type='submit'>Submit</button>
+            </form>
+            <div className='taskContainer'>
+                {task.map((currentTask, index) => (
+                    <h1 key={index}>{currentTask}</h1>
+                ))}
+            </div>
+        </div>
+    )
+
 }
 
 export default App;
